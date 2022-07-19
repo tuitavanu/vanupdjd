7fc9e0ce44063a0041944612f3bd88d9e22577fd
Vanuza Cacheado de Moura <vanuza.moura@academico.ifrn.edu.br>
2022-07-19 15:34:21 -0300

    aula 12/07

---
+++ PDJD/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Scene = UnityEngine.SceneManagement.Scene;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject playerAndCameraPrefab;
+
+    [SerializeField] private string locationToLoad;
+
+    [SerializeField] private string guiScene;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        SceneManager.LoadScene(guiScene);
+        //SceneManager.LoadScene(locationToLoad, LoadSceneMode.Additive);
+
+        SceneManager.LoadSceneAsync(locationToLoad, LoadSceneMode.Additive).completed += operation =>
+        {
+            Vector3 startPosition = GameObject.Find("PlayerStart").transform.position;
+
+            Instantiate(playerAndCameraPrefab, startPosition, Quaternion.identity);
+        };
+    }
+}
